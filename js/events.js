6e81cd498a93fc65fbd3c76ba5c26f2787420adb
Lorenzo Martinez <lmartinez914@gmail.com>
2018-07-01 04:07:46 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
 
+function getIT(){
+  alert ('Hey!');
+  
+}
+  function frameIT() {
+    $("img").addClass("tasty");
+  }
+  function pressIt(){
+    $("form").on("keydown",function(key) {
+      if (key.which === 71 ) {
+        alert ('You have pressed the G key');
+      }
+    });
+  }
+  
+  function submitIt() {
+    $("form").on("submit", function (){
+      alert ('Your form will be submitted now.');
+    });
+  }
 $(document).ready(function(){
 
 // call functions here
-
+$("p").on(click,getIt());
+$("img").on("load",frameIt());
+pressIt();
 });
